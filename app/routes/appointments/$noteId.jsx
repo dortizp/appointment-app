b4e4d541c4185fdeef88fc59f752d0ba6dc43ca2
David Ortiz <david.ortiz.p@gmail.com>
2023-02-22 14:59:28 -0500

    appointment body show from data

---
+++ app/routes/appointments/$noteId.jsx
@@ -0,0 +1,38 @@
+// import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { deleteNote, getNote } from "~/models/note.server";
+import { requireUserId } from "~/session.server";
+
+export async function loader ({request, params}) {
+    // return json({id: params.appointmentId})
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  const note = await getNote({ userId, id: params.noteId });
+  if (!note) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  console.log(note)
+  return json({ note });
+}
+
+export default function Appointment() {
+    const data = useLoaderData()
+    return (
+        <div>
+            <h1>Appointment</h1>
+        <p>Here we will show an appointment</p>
+
+      {data? (
+        <p>{data.note.body}</p>
+      ):
+        <p>no notes</p>
+      }
+        {/* <p>This appointment has id {data.note.id}</p> */}
+        {/* <p>detail: {data.note.body}</p> */}
+        </div>
+    )
+}
